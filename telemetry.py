71da65b769f91ba518597c624720b76fa08fbc51
STeven L1i <niceStevenLi@outlook.com>
2022-04-29 04:43:12 +0800

    general debug

--- telemetry.py
+++ telemetry.py
@@ -23,6 +23,7 @@ while True:
     print(f'laptime file {filepath} imported')
 
 
+############## throttle plot ###############
 fig, ax = plt.subplots(figsize=(64,16))
 
 plt.ylim(0,1.05)
@@ -47,9 +48,10 @@ for filepath in telefile:
             if row.get("name") == "":
                 continue
             name = row.get("name")
-            distance = float(f'{(float(row.get("LapDistance"))):.4f}')
+            distance = float(f'{float(row.get("LapDistance")):.4f}')
             LapDistance.append(distance)
-            throttle.append(float(f'{float(row.get("throttle")):.4f}'))
+            th = float(f'{float(row.get("throttle")):.4f}')
+            throttle.append(th)
 
         ax.plot(LapDistance, throttle, label=(f'{name}'), linewidth=2)
 
@@ -63,7 +65,7 @@ plt.savefig("telemetrydata/telemetry(power).png", format="png")
 
 
 
-
+############## braking plot ###############
 fig2, ax2 = plt.subplots(figsize=(64,16))
 
 plt.ylim(0,1.05)
@@ -90,7 +92,8 @@ for filepath in telefile:
             name = row.get("name")
             distance = float(f'{float(row.get("LapDistance")):.4f}')
             LapDistance.append(distance)
-            brake.append(float(f'{float(row.get("brake")):.4f}'))
+            b = float(f'{float(row.get("brake")):.4f}')
+            brake.append(b)
 
         ax2.plot(LapDistance, brake, label=(f'{name}'), linewidth=2)
 
@@ -99,3 +102,45 @@ plt.grid(True)
 ax2.legend(frameon=True)
 
 plt.savefig("telemetrydata/telemetry(brake).png", format="png")
+
+
+
+
+
+############## gear plot ###############
+fig3, ax3 = plt.subplots(figsize=(64,16))
+
+plt.ylim(0,9)
+
+ax3.xaxis.set_major_locator(plt.MultipleLocator(100))
+ax3.yaxis.set_major_locator(plt.MultipleLocator(1))
+
+ax3.tick_params(axis="x", bottom=True, top=True, labelbottom=True, labeltop=False, pad=2)
+
+ax3.set_title("Distance", loc="left")
+ax3.set_xlabel("Lap Distance")
+ax3.set_ylabel("gear")
+
+for filepath in telefile:
+    name = ""
+    with open(filepath, "r") as tele:
+        reader = csv.DictReader(tele)
+
+        LapDistance = []
+        gear = []
+        for row in reader:
+            if row.get("name") == "":
+                continue
+            name = row.get("name")
+            distance = float(f'{float(row.get("LapDistance")):.4f}')
+            LapDistance.append(distance)
+            g = int(row.get("gear"))
+            gear.append(g)
+
+        ax3.plot(LapDistance, gear, label=(f'{name}'), linewidth=2)
+
+
+plt.grid(True)
+ax3.legend(frameon=True)
+
+plt.savefig("telemetrydata/telemetry(gear).png", format="png")
